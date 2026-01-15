9d1a7a9487ca03fe1370fbafb637ad80b418c827
Aneesh <creative@phitany.com>
2026-01-15 13:45:50 +0530

    html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <h1 className="text-2xl font-semibold mb-6 bg-red-500">Home Page</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-
+        hello
       </div>
       <div className="bg-red-500 text-white p-5">
         Tailwind working
