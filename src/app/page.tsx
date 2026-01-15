5c7eac65d5bb6bf8b495ba9d784364e0864529f6
Aneesh <creative@phitany.com>
2026-01-15 13:44:13 +0530

    html updation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,19 @@
 
 
 export default function Home() {
- 
+
 
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-semibold mb-6">Home Page</h1>
+    <main className="p-0">
+      <h1 className="text-2xl font-semibold mb-6 bg-red-500">Home Page</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-       
+
+      </div>
+      <div className="bg-red-500 text-white p-5">
+        Tailwind working
       </div>
     </main>
+
   );
 }
