fff088d7532e8b0ba4d65e25b168b7a44ee27a71
Aneesh <creative@phitany.com>
2026-01-22 14:22:50 +0530

    html pushed

--- src/components/home/SpecialityTours.tsx
+++ src/components/home/SpecialityTours.tsx
@@ -45,7 +45,7 @@ export default function SpecialityTours() {
                             </div>
                         </div>
 
-                        <div className="w-full md:w-1/4 ml-0 md:pl-10">
+                        <div className="w-full md:w-1/4 ml-0 md:pl-10 relative">
                             <h3 className=" font-my-font-regular text-4xl text-(--color-secondary)  text-left">Speciality <br /> Tours</h3>  <div className="absolute left-0 top-0  w-px h-10 bg-gray-400 "></div></div>
                     </div>
                 </div>
