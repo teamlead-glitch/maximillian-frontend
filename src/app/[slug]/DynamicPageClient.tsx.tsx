d21d3fbae79cfc179a262f63bb0df3b3645db9b8
Sherin Reynold <teamlead@phitany.com>
2026-02-05 17:42:19 +0530

    dynamic content pages

---
+++ src/app/[slug]/DynamicPageClient.tsx.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { PageResponse } from "@/types/pagesTypes";
+import NotFound from "../not-found";
+
+
+export default function DynamicPageClient({
+  page
+}: {
+  page: PageResponse 
+}) {
+  
+
+
+  
+
+
+  if (!page) {
+    return <NotFound />; // ✅ NO redirect, default UI
+  }
+
+  return (
+    <>
+
+
+     {/* HEADER */}
+            <section
+                className="relative py-40 bg-black bg-cover bg-center"
+                style={{
+                    backgroundImage: "url('/images/inner-bg.jpg')",
+                }}
+            >
+                {/* overlay */}
+                <div className="absolute inset-0 bg-black/30"></div>
+
+                <h1 className="relative z-10 text-center text-white text-4xl font-my-font-semibold">
+                    {page?.title}
+                </h1>
+            </section>
+
+
+
+            {/* CONTENT */}
+            <section className="relative py-20 bg-white ">
+
+               <div
+            className="prose max-w-none description-content"
+            style={{ minHeight: "150px" }}
+            dangerouslySetInnerHTML={{ __html: page?.content ?? '' }}
+          />
+        
+               
+            </section >
+     
+
+
+
+
+     
+
+
+    </>
+  );
+}
---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,49 @@
+import { generateSeoMetadata } from "@/lib/seo";
+import DynamicPageClient from "./DynamicPageClient.tsx";
+import { fetchPageBySlug } from "@/lib/page-api";
+import NotFound from "@/app/not-found";
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await params; // ✅ UNWRAP PARAMS
+
+  if (!slug || slug === "home") {
+    return {};
+  }
+
+  return generateSeoMetadata(slug);
+};
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const {slug} = await params; 
+   const page = await fetchPageBySlug(slug);
+
+     if (!page ||  slug == 'home') {
+       // Optional: you can throw notFound() here
+       return <NotFound />;
+     }
+
+   return (
+    <>
+      {/* ✅ JSON-LD SCHEMA (SERVER RENDERED) */}
+      {page?.seo?.schema_markup && (
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: page.seo.schema_markup,
+          }}
+        />
+      )}
+
+      <DynamicPageClient page={page}/>
+    </>
+  );
+  
+ 
+}
