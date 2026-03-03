9c189b9108498e20ff69d85c5e70df881b44eca2
Sherin Reynold <teamlead@phitany.com>
2026-03-03 17:12:11 +0530

    navigation issue fix

--- src/components/common/header/menu/TopMenuinner.tsx
+++ src/components/common/header/menu/TopMenuinner.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect, useRef } from "react";
 import SideBarMenuInner from "./../SideBarMenuInner";
 import { RegionFormated } from "@/lib/regionTransformer";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 
 /* ================= COMPONENT ================= */
@@ -20,6 +21,9 @@ export default function TopMenuinner({ regions }: { regions: Record<string, Regi
   );
 
     const megaMenuRef = useRef<HTMLDivElement>(null);
+
+     const pathname = usePathname();
+
 
     /* ---------------- DISABLE BODY SCROLL ---------------- */
     useEffect(() => {
@@ -68,6 +72,10 @@ export default function TopMenuinner({ regions }: { regions: Record<string, Regi
             document.removeEventListener("mousedown", handleClickOutside);
         };
     }, [megaOpen]);
+
+      useEffect(() => {
+    setMegaOpen(false);
+  }, [pathname]); // 👈 runs on every route change
 
     return (
         <>
@@ -186,11 +194,11 @@ export default function TopMenuinner({ regions }: { regions: Record<string, Regi
 
                                         <ul className="grid grid-cols-2 gap-3">
                                             {regions[activeRegion]?.countries?.length > 0 && regions[activeRegion].countries.map((country) => (
-                                                <Link  href={`/${country?.slug}`} onClick={() => setMegaOpen(false)} key={country.title} className="cursor-pointer hover:text-[#C43131] transition-all duration-300">{country.title}</Link>
+                                                <Link  href={`/${country?.slug}`}  key={country.title} className="cursor-pointer hover:text-[#C43131] transition-all duration-300">{country.title}</Link>
                                             ))}
                                         </ul>
 
-                                        <Link  href={`/${regions[activeRegion]?.slug}`} onClick={() => setMegaOpen(false)} className="mt-8 border border-[#C43131] block w-fit px-6 py-2 rounded-full text-sm hover:bg-[#C43131] cursor-pointer hover:text-white hover:tracking-wide transition-all duration-300">
+                                        <Link  href={`/${regions[activeRegion]?.slug}`}  className="mt-8 border border-[#C43131] block w-fit px-6 py-2 rounded-full text-sm hover:bg-[#C43131] cursor-pointer hover:text-white hover:tracking-wide transition-all duration-300">
                                             View all journeys in {activeRegion}
                                         </Link>
                                     </div>
