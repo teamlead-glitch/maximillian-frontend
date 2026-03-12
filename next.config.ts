1ec542fc11856c968c3ca0e500e59bacc5a130f1
Sherin Reynold <teamlead@phitany.com>
2026-03-12 14:02:03 +0530

    vercel build only config updates

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,27 @@
+// import type { NextConfig } from "next";
+
+// const apiUrl = process.env.NEXT_PUBLIC_API_URI || "";
+// const hostname = new URL(apiUrl).hostname;
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: hostname,
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URI || "";
-const hostname = new URL(apiUrl).hostname;
-
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: hostname,
-      },
-    ],
+  /* config options here */
+   images: {
+    domains: [ "steelblue-cheetah-101023.hostingersite.com"],
   },
 };
 
