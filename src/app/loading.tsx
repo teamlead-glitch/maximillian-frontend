a071358f3e7d7a8052fca37f644a4bfbc3673227
Sherin Reynold <teamlead@phitany.com>
2026-03-13 10:33:11 +0530

    common global loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import Loader from "@/components/common/Loader";
+
+export default function Loading() {
+  return (
+    <div className="h-[70vh] flex items-center justify-center">
+      <Loader />
+    </div>
+  );
+}
