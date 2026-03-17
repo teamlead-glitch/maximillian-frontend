861553b98b646fa45d44584a7ff1ebfe3cafba17
Sherin Reynold <teamlead@phitany.com>
2026-03-17 12:37:00 +0530

    hostinger only config changes revert

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,17 @@
 import type { NextConfig } from "next";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URI;
-
-let hostname: string | undefined;
-try {
- hostname = apiUrl ? new URL(apiUrl).hostname : undefined;
-} catch {
- hostname = undefined;
-}
+const apiUrl = process.env.NEXT_PUBLIC_API_URI || "";
+const hostname = new URL(apiUrl).hostname;
 
 const nextConfig: NextConfig = {
- output: "standalone",
- images: {
- remotePatterns: hostname
- ? [{ protocol: "https", hostname }]
- : [],
- },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: hostname,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-
-
-//hided below code for hostinger node app deploying issue fix and added above code 17-03-2026
-// import type { NextConfig } from "next";
-
-// const apiUrl = process.env.NEXT_PUBLIC_API_URI || "";
-// const hostname = new URL(apiUrl).hostname;
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: hostname,
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
