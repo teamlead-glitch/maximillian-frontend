8efe5ff07b888e7d9eee00f1dc72a50d82ff9c7d
Sherin Reynold <teamlead@phitany.com>
2026-03-25 16:41:48 +0530

    robots disallow to allow

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -13,7 +13,9 @@ export default async function robots(): Promise<MetadataRoute.Robots> {
     rules: [
       {
         userAgent: "*",
-        disallow: "/",
+        // disallow: "/",
+        allow: "/",
+
       },
     ],
     sitemap: `${siteUrl}/sitemap.xml`,
