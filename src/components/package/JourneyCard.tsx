294f4221b2b50776dc97ed720b8960af3b1e6f63
Sherin Reynold <teamlead@phitany.com>
2026-04-01 14:49:02 +0530

    signature card whole clickable

--- src/components/package/JourneyCard.tsx
+++ src/components/package/JourneyCard.tsx
@@ -11,6 +11,7 @@ interface Props {
 export default function JourneyCard({ journey }: Props) {
     return (
         <div className="div">
+            <Link href={`/package/${journey.short_slug}`}>
             <div className="group block">
                 <div className="rounded-md relative aspect-[1/1] overflow-hidden">
 
@@ -24,7 +25,7 @@ export default function JourneyCard({ journey }: Props) {
                     {/* Bottom Content */}
                     <div className=" absolute w-full bottom-0 left-0 p-5 flex flex-col items-center transition-transform duration-500 ease-out group-hover:-translate-y-5">
                         <h3 className="font-my-font-regular text-white text-2xl text-center">
-                            <Link href={`/package/${journey.short_slug}`}>{journey.title}</Link>
+                            {journey.title}
                         </h3>
 
                         <TagListingForSignature
@@ -32,30 +33,12 @@ export default function JourneyCard({ journey }: Props) {
                             region={journey.region}
                         />
 
-                        {/* <div className="text-white text-xs">
-                            <ul className="flex flex-wrap justify-center items-center gap-2 mt-1">
-                                <li className="relative pr-3 after:content-['•'] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:text-white">
-                                    <Link href={`/region/${journey.region.short_slug}`}>{journey.region.title}</Link>
-                                </li>
-
-                                {journey.tags.map((tag, index) => (
-                                    <li
-                                        key={tag.id}
-                                        className={
-                                            index !== journey.tags.length - 1
-                                                ? "relative pr-3 after:content-['•'] after:absolute after:right-0 after:top-1/2 after:-translate-y-1/2 after:text-white"
-                                                : "relative"
-                                        }
-                                    >
-                                        <Link href={`/tag/${tag.short_slug}`}>{tag.title}</Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div> */}
+                       
                     </div>
 
                 </div>
             </div>
+            </Link>
         </div>
     );
 }
