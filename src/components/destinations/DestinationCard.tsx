ae28ad7e4a4b7b8e877675a224698a2f2545d1f9
Aneesh <creative@phitany.com>
2026-04-28 07:52:15 +0530

    updated components

--- src/components/destinations/DestinationCard.tsx
+++ src/components/destinations/DestinationCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function DestinationCard({ destination }: Props) {
   return (
-    <Link href={`/country/${destination.short_slug}`} className="bg-white rounded-xl overflow-hidden transition-all duration-300 my-4 md:my-4 hover:shadow-2xl group">
+    <Link href={`/country/${destination.short_slug}`} className="bg-white rounded-xl overflow-hidden transition-all duration-300 my-4 md:my-4 shadow-xl hover:shadow-2xl group border border-gray-200">
       {/* Image */}
       <div className="w-full aspect-3/3 overflow-hidden relative">
         <Image
@@ -19,18 +19,18 @@ export default function DestinationCard({ destination }: Props) {
           width="650"
           height="650"
         />
-         {/* 📱 Mobile only text over image */}
-  <div className="absolute inset-x-0 bottom-0 md:hidden">
-  <div className="bg-gradient-to-t from-black/80 via-black/40 to-transparent px-3 py-3">
-    <p className="text-white text-sm line-clamp-2">
-      {destination.short_description}
-    </p>
-  </div>
-</div>
+        {/* 📱 Mobile only text over image */}
+        <div className="absolute inset-x-0 bottom-0 md:hidden">
+          <div className="bg-gradient-to-t from-black/80 via-black/40 to-transparent  px-3 py-3">
+            <p className="text-white line-clamp-2 pb-0 text-xs mb-0">
+              {destination.short_description}
+            </p>
+          </div>
+        </div>
       </div>
 
       {/* Content */}
-      <div className="py-5 px-6 text-center relative h-full flex flex-col md:min-h-[120px]">
+      <div className="py-2 md:py-5 px-6 text-center relative h-full flex flex-col md:min-h-[120px]">
         {/* <div
           className="absolute -top-[40px] right-4 z-10 w-12 h-12 rounded-full flex items-center justify-center bg-white/20 backdrop-blur-lg transition-all duration-300 hover:bg-white/30 hover:scale-105 cursor-pointer">
           <Image
@@ -42,11 +42,11 @@ export default function DestinationCard({ destination }: Props) {
           />
         </div> */}
 
-        <h3 className="text-lg font-semibold mt-1 font-my-font-regular text-(--color-secondary) line-clamp-1">
+        <h3 className="text-base md:text-lg font-semibold mt-1 font-my-font-regular text-(--color-secondary) line-clamp-1">
           {destination.title}
         </h3>
 
-        <p className="hidden md:block text-sm text-gray-600 mt-2 line-clamp-2">
+        <p className="hidden md:block text-xs text-gray-600 mt-2 line-clamp-2">
           {destination.short_description}
         </p>
       </div>
