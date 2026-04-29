ff78db0c044e4ff4d623f5c951660b25c45f325e
Sherin Reynold <teamlead@phitany.com>
2026-04-29 12:16:51 +0530

    country landing page mobile banner not found show desktop banner

--- src/components/countrylanding/Countrylist.tsx
+++ src/components/countrylanding/Countrylist.tsx
@@ -99,7 +99,7 @@ export default function Countrylist({ slug, regionDetails }: { slug: string; reg
             <section className="relative  pt-15 pb-0 md:pt-20 overflow-hidden bg-white">
                 <div className="w-full relative  aspect-[16/9]   md:aspect-[16/4]">
 
-                    <Image fill src={regionDetails?.banner_mob_image ?? ''} alt={regionDetails?.title ?? ''} className="w-full h-full object-cover  md:hidden" />
+                    <Image fill src={regionDetails?.banner_mob_image ? regionDetails?.banner_mob_image : regionDetails?.banner_image ?? ''} alt={regionDetails?.title ?? ''} className="w-full h-full object-cover  md:hidden" />
                     <Image fill src={regionDetails?.banner_image ?? ''} alt={regionDetails?.title ?? ''} className="w-full h-full object-cover hidden md:block" />
 
                     <div className="absolute inset-0 bg-black/20"></div>
