4b0ee3bf624cc0b3f417922c1b5a17daedd78ffe
Sherin Reynold <teamlead@phitany.com>
2026-06-16 11:45:05 +0530

    button style update in cuntry list page

--- src/components/countrylanding/Countrylist.tsx
+++ src/components/countrylanding/Countrylist.tsx
@@ -246,7 +246,22 @@ px-4 sm:px-0 text-center">
                                 onClick={() => setShowAll(!showAll)}
                                 className="group flex items-center font-my-font-semibold text-black text-sm sm:text-base justify-center py-3 mt-0 md:mt-3 cursor-pointer"
                             >
-                                {showAll ? "View Less Journeys" : "View More Journeys"}
+                                <span className="mr-3">
+                                    {showAll ? "View Less Journeys" : "View More Journeys"}
+                                </span>
+                                <svg
+                                    className="transition-transform duration-300 ease-out group-hover:translate-x-[10px]"
+                                    width="53"
+                                    height="8"
+                                    viewBox="0 0 53 8"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M52.3536 4.03556C52.5488 3.8403 52.5488 3.52372 52.3536 3.32845L49.1716 0.146473C48.9763 -0.0487893 48.6597 -0.0487893 48.4645 0.146473C48.2692 0.341735 48.2692 0.658318 48.4645 0.85358L51.2929 3.68201L48.4645 6.51043C48.2692 6.7057 48.2692 7.02228 48.4645 7.21754C48.6597 7.4128 48.9763 7.4128 49.1716 7.21754L52.3536 4.03556ZM0 3.68201V4.18201H52V3.68201V3.18201H0V3.68201Z"
+                                        fill="#3A3F42"
+                                    />
+                                </svg>
                             </button>
                         </div>
                     )}
