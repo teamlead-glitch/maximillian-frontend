2e3c134cae01f13b8aed3013b7c6de54f60957b5
Sherin Reynold <teamlead@phitany.com>
2026-06-27 12:13:52 +0530

    /blogs/womens-only-trip-new-orleans-friendship-travel-story temp redirect to new url

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,17 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URI || "";
 const hostname = new URL(apiUrl).hostname;
 
 const nextConfig: NextConfig = {
+
+  async redirects() {
+    return [
+      {
+        source: "/blogs/womens-only-trip-new-orleans-friendship-travel-story",
+        destination:
+          "/blogs/where-friendship-found-its-rhythm-a-women-s-escape-to-new-orleans",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
