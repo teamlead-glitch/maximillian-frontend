84729656de71f9e08453aafabef54fa4e5f4d8a4
Sherin Reynold <teamlead@phitany.com>
2026-06-30 16:18:12 +0530

    whatmake region exp updates

--- src/components/regionlanding/Regionlist.tsx
+++ src/components/regionlanding/Regionlist.tsx
@@ -309,7 +309,7 @@ px-4 sm:px-0 text-center">
 
 
 
-            <div className="content what-makes-exceptional bg-white px-5 md:px-0" dangerouslySetInnerHTML={{ __html: regionDetails?.what_makes_us_exceptional || "" }}></div>
+            <div className="content what-makes-exceptional bg-white px-3 md:px-0" dangerouslySetInnerHTML={{ __html: regionDetails?.what_makes_us_exceptional || "" }}></div>
             {/* <section className="bg-white py-10">
                 <div className="max-w-[1300px] flex flex-col md:flex-row items-start md:items-center gap-4 md:gap-8 mx-auto  px-5  ">
                     <div className="inline-block]">
